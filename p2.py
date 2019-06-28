844fb04cae176b5e244d84d6ddf6690a0edcf752
root <gvelayutham@fleetstudio.com>
2019-06-28 17:58:25 +0000

    react

---
+++ p2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python2
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+import cgi
+
+class Server(BaseHTTPRequestHandler):
+
+    def _send_cors_headers(self):
+     self.send_header("Access-Control-Allow-Origin" , "*")
+     self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+     self.send_header("Access-Control-Allow-Headers", "x-api-key,Content-Type")
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "x-api-key,Content-Type")
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    # GET sends back a Hello world message
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'message': 'world', 'received': 'ok'}))
+        
+    # POST echoes the message adding a JSON field
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+            
+        # read the message and convert it into a python dictionary
+        length = int(self.headers.getheader('content-length'))
+        message = json.loads(self.rfile.read(length))
+        
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+        
+        # send the message back
+        self._set_headers()
+        self.wfile.write(json.dumps(message))
+        
+def run(server_class=HTTPServer, handler_class=Server, port=8008):
+    server_address = ('0.0.0.0', port)
+    httpd = server_class(server_address, handler_class)
+    
+    print 'Starting httpd on port %d...' % port
+    httpd.serve_forever()
+    
+if __name__ == "__main__":
+    from sys import argv
+    
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+        
